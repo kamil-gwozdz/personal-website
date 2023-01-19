cb84522be0a3dd2b1fa3a2cae197a5fc0061577e
Kamil Gwóźdź <kamil-gwozdz@github.com>
2023-01-19 14:59:14 +0100

    fix

--- src/routes/guest_list/+page.svelte
+++ src/routes/guest_list/+page.svelte
@@ -1,2 +1,6 @@
+<script>
+  import Image from "../../assets/img/under_construction.gif"
+</script>
+
 <h1>Under Construction</h1>
-<img src="../../src/assets/img/under_construction.gif" alt="under construction">
+<img src="{Image}" alt="under construction">
