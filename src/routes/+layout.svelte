337d5dc67903ba7497ad694306b5a4e0ea8cb109
Kamil Gwóźdź <kamil-gwozdz@github.com>
2023-01-19 14:59:10 +0100

    add rainbow

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,18 @@
+<script context="module">
+  import { rainbowCursor } from 'cursor-effects';
+</script>
+
 <script lang="ts">
+  import { onMount } from "svelte";
+
   import Footer from "$lib/components/Footer.svelte";
   import Navbar from "$lib/components/Navbar.svelte";
   import Header from "$lib/components/header/Header.svelte";
   import "../styles.css"
+
+  onMount(() => {
+    new rainbowCursor({length: 35, size: 5});
+  });
 </script>
 
 <Header />
