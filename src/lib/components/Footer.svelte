9b6c5817190d4a9cc7f503c625e53581bcfd3614
Kamil Gwóźdź <kamil-gwozdz@github.com>
2023-01-19 12:13:34 +0100

    svelte kit

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,26 @@
+<footer>
+  <marquee><p>Kamil Gwóźdź © 2023</p></marquee>
+</footer>
+
+<style>
+    footer {
+        background-color: #777;
+        text-align: center;
+        color: white;
+        height: 6vh;
+        position: absolute;
+        width: 100%;
+        bottom: 0;
+        display: flex;
+        flex-wrap: wrap;
+        align-content: center;
+    }
+
+    footer marquee p {
+        margin: 0;
+    }
+
+    footer marquee {
+        padding: 10px;
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Footer from "$lib/components/Footer.svelte";
+  import Header from "$lib/components/header/Header.svelte";
+  import "../styles.css"
+</script>
+
+<Header />
+<slot></slot>
+<Footer />
